4445b1861f6edf98abc334c7fbb11f76ecfadc8f
Katie Chai <kchai@unc.edu>
2023-11-09 15:17:58 -0500

    timer script

--- README.md
+++ README.md
@@ -6,3 +6,8 @@ Script to swich between panels of main menu! I used code from this Youtube Video
 
 ## build-bar & entities
 Script filter and show entities by group name! I used code from this Youtube Video ([https://www.youtube.com/watch?v=kSL-KjlRAPc]) by Mubanga.
+
+## timer
+Script for countdown timer! I used code from this Youtube Video ([https://www.youtube.com/watch?v=o0j7PdU88a4]) by Single Sapling Games.
+
+Here is the Time.deltaTime document that was referenced ([https://docs.unity3d.com/ScriptReference/Time-deltaTime.html]).
---
+++ timer/CountdownTimer.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Needs to countdown from a certain time = game duration
+public class CountdownTimer : MonoBehaviour
+{
+    float currentTime = 0f;
+    float startingTime = 10f;
+
+    void Start()
+    {
+        // Once the game starts, setting current time to starting time
+        currentTime = startingTime;
+    }
+
+    void Update()
+    {
+        // currentTime -= 1 does by each frame...bad
+        currentTime -= 1 * currentTime.deltaTime; // decreases by 1 each second instead
+        print (currentTime);
+    }
+}
